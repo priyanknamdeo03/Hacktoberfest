26cf57cb6243f54284b86fc16092b570a21e4ea8
priyanknamdeo03 <priyanknamdeo03@gmail.com>
2021-10-25 19:07:20 +0530

    adding firebase.py

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDpgkXeGRznKv2VD8xRF5ls_5JqOkB17rs",
+    authDomain: "workshopproject-53472.firebaseapp.com",
+    projectId: "workshopproject-53472",
+    storageBucket: "workshopproject-53472.appspot.com",
+    messagingSenderId: "679910305555",
+    appId: "1:679910305555:web:ba79a49956da7adedaf4e6"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
